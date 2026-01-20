b323d7f167cc19a93322bfb53cf6107950f6faaa
phan_hoang_quan <phanhoangquan123@gmail.com>
2026-01-20 13:38:39 +0700

    Validate Form React

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import './App.scss';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Bounce } from 'react-toastify';
 import Home from './pages/Home/Home';
-
 
 import Nav from './components/Navigation/Nav';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 
-
 function App() {
-  return (
-  <div className='app-container'>
-    <BrowserRouter>
-         <Nav />
-         <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              {/* <Route path="/news" element={<News />} />
+   return (
+      <div className="app-container">
+         <BrowserRouter>
+            <Nav />
+            <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="/login" element={<Login />} />
+               <Route path="/register" element={<Register />} />
+               {/* <Route path="/news" element={<News />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} /> */}
-              {/* <Route path="*"><h1>404 Not Found!</h1></Route> */}
-
-        </Routes>
-    </BrowserRouter>
-  </div>
-);
+               {/* <Route path="*"><h1>404 Not Found!</h1></Route> */}
+            </Routes>
+            <ToastContainer
+               position="top-right"
+               autoClose={5000}
+               hideProgressBar={false}
+               newestOnTop={false}
+               closeOnClick={false}
+               rtl={false}
+               pauseOnFocusLoss
+               draggable
+               pauseOnHover
+               theme="light"
+               transition={Bounce}
+            />
+         </BrowserRouter>
+      </div>
+   );
 }
 export default App;
