11ea83fa36e27888153d1398f26b2bd6d261b03b
phan_hoang_quan <phanhoangquan123@gmail.com>
2026-01-22 19:47:31 +0700

    Validate Login , Post data to Backend

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,33 +1,83 @@
-import './Login.scss'
+import { useState } from 'react';
+import './Login.scss';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { loginUser } from '../../services/userServices';
 
 function Login() {
-    return (<div className="login-container">
-        <div className="container">
+   const [valueLogin, setValueLogin] = useState('');
+   const [password, setPassword] = useState('');
+
+   const defaultobjValidInputs = {
+      valueLogin: true,
+      password: true,
+   };
+   const [objIsValidInputs, setObjIsValidInputs] = useState(defaultobjValidInputs);
+
+   const handleLogin = async () => {
+      setObjIsValidInputs(defaultobjValidInputs);
+      if (!valueLogin) {
+         setObjIsValidInputs({ ...defaultobjValidInputs, valueLogin: false });
+         toast.error('Email or phone number is required');
+         return;
+      }
+      if (!password) {
+         setObjIsValidInputs({ ...defaultobjValidInputs, password: false });
+         toast.error('Password is required');
+         return;
+      }
+      await loginUser(valueLogin, password);
+   };
+
+   return (
+      <div className="login-container">
+         <div className="container">
             <div className="row px-3 px-sm-0">
-                <div className="content-left d-none col-sm-7 d-sm-block">
-                    <div className='brand'>
-                        BRAND
-                    </div>
-                    <div className='detail'>
-                        BRAND is website for ...
-                    </div>
-                </div>
-                
-                <div className="content-right col-12 col-sm-5 d-flex flex-column gap-3">
-                    <div className='brand d-sm-none'>
-                        BRAND
-                    </div>
-                    <input type='text' className='form-control' placeholder='Email Address or phone number'/>
-                    <input type='text' className='form-control' placeholder='Password'/>
-                    <button className='btn btn-primary'>Login</button>
-                    <span className='text-center'><Link className='forgot-password' href='#'>Forgot your password?</Link></span>
-                    <hr/>
-                    <button className='btn btn-success'><Link to="/register" style={{'textDecoration': 'none','color':'white'}}>Create new account</Link></button>
-                </div>
+               <div className="content-left d-none col-sm-7 d-sm-block">
+                  <div className="brand">BRAND</div>
+                  <div className="detail">BRAND is website for ...</div>
+               </div>
+
+               <div className="content-right col-12 col-sm-5 d-flex flex-column gap-3 py-4">
+                  <div className="brand d-sm-none">BRAND</div>
+                  <input
+                     type="text"
+                     className={objIsValidInputs.valueLogin ? 'form-control' : 'form-control is-invalid'}
+                     placeholder="Email Address or phone number"
+                     value={valueLogin}
+                     onChange={(e) => setValueLogin(e.target.value)}
+                  />
+                  <input
+                     type="text"
+                     className={objIsValidInputs.password ? 'form-control' : 'form-control is-invalid'}
+                     placeholder="Password"
+                     value={password}
+                     onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <button
+                     className="btn btn-primary"
+                     onClick={() => {
+                        handleLogin();
+                     }}
+                  >
+                     Login
+                  </button>
+                  <span className="text-center">
+                     <Link className="forgot-password" href="#">
+                        Forgot your password?
+                     </Link>
+                  </span>
+                  <hr />
+                  <button className="btn btn-success">
+                     <Link to="/register" style={{ textDecoration: 'none', color: 'white' }}>
+                        Create new account
+                     </Link>
+                  </button>
+               </div>
             </div>
-        </div>
-    </div>  );
+         </div>
+      </div>
+   );
 }
 
 export default Login;
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const registerNewUser = (email, username, phone, password) => {
    return axios.post('http://localhost:8080/api/v1/register', { email, username, phone, password });
 };
+
+export const loginUser = (valueLogin, password) => {
+   return axios.post('http://localhost:8080/api/v1/login', { valueLogin, password });
+};
